893437c63581bea07e4ca799be189b0fe44ebc94
Yurii <fanfjrfj589@gmail.com>
2024-01-04 19:53:56 +0100

    add swiper co course section

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -2,6 +2,16 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import "./style/Course.css";
 import CourseCard from '../UI/CourseCard/CourseCard';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Keyboard, Pagination, Navigation } from 'swiper/modules';
 
 const Course = ({ courseArr }) => {
 	const { t,} = useTranslation();
@@ -10,14 +20,46 @@ const Course = ({ courseArr }) => {
       <p className='course__title'>{t("CoursePage.title")}</p>
       <div className="course__bg"></div>
 
+
       <div className="course__box">
-        {courseArr ? (
+        
+        <Swiper
+          slidesPerView={3}
+          keyboard={{
+            enabled: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Keyboard, Pagination, Navigation]}
+          className="mySwiper"
+          breakpoints={{
+            1350: {
+              slidesPerView: 3,
+            },
+            1000: {
+              slidesPerView: 2,
+            },
+            500: {
+              slidesPerView: 1,
+            },
+            100: {
+              slidesPerView: 1,
+            },
+          }}
+        >
+          {courseArr ? (
           courseArr.map((course, index) => (
-            <CourseCard key={index} index={index} courseArr={course}/>
+            <SwiperSlide key={index}>
+              <CourseCard index={index} courseArr={course} />
+            </SwiperSlide>
           ))
-        ) : (
-          <p>Loading...</p>
-        )}
+          ) : (
+            <p>Loading...</p>
+          )}
+        </Swiper>
+   
       </div>
     </div>
   );
