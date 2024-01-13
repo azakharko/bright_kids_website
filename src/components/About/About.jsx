c620fb17600a722b86337f7d2981bbe422c0ca51
Yurii <fanfjrfj589@gmail.com>
2024-01-13 15:04:03 +0100

    add LazyLoad to About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './style/About.css';
+import LazyLoad from 'react-lazy-load';
 
 const About = () => {
   const { t } = useTranslation(); // useTranslation hook
@@ -10,15 +11,17 @@ const About = () => {
       <div className="about-us__title">{t('About.title')}</div>
       <div className="about-us__description">{t('About.description')}</div>
 
-      <div className="about-us__video">
-        <iframe
-          src="https://www.youtube.com/embed/d36UClPy7us?si=VJ7wMdry6QAyNxgL"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-      </div>
+      <LazyLoad width={"100%"} offset={100}  >
+        <div className="about-us__video" >
+          <iframe style={{ zIndex: 2 }}
+            src="https://www.youtube.com/embed/d36UClPy7us?si=VJ7wMdry6QAyNxgL"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </LazyLoad>
     </div>
   );
 }
