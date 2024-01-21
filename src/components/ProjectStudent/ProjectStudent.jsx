997b7605fbcdd28fa4043ff88f22cfac60343f76
Yurii <fanfjrfj589@gmail.com>
2024-01-21 18:59:50 +0100

    add python projects

--- src/components/ProjectStudent/ProjectStudent.jsx
+++ src/components/ProjectStudent/ProjectStudent.jsx
@@ -16,7 +16,22 @@ import img9 from "./img/img9.png";
 import img10 from "./img/img10.png";
 import img12 from "./img/img12.png";
 import img13 from "./img/img13.png";
-import img14 from "./img/img14.png";
+
+
+import python1 from "./img/python/python1.png";
+import python2 from "./img/python/python2.png";
+import python3 from "./img/python/python3.png";
+import python4 from "./img/python/python4.jpg";
+import python5 from "./img/python/python5.jpg";
+import python6 from "./img/python/python6.jpg";
+import python7 from "./img/python/python7.png";
+import python8 from "./img/python/python8.jpg";
+import python9 from "./img/python/python9.png";
+import python10 from "./img/python/python10.png";
+import python11 from "./img/python/python11.png";
+import python12 from "./img/python/python12.png";
+import python13 from "./img/python/python13.png";
+import python14 from "./img/python/python14.png";
 
 
 const ProjectStudent = ({ setBurgerOpen, burgerOpen }) => {
@@ -68,7 +83,28 @@ const ProjectStudent = ({ setBurgerOpen, burgerOpen }) => {
 				<img src={img10} alt="img" />
 				<img src={img12} alt="img" />
 				<img src={img13} alt="img" />
-				<img src={img14} alt="img" />
+			</div>
+
+			<div className="gallery__box-title">
+				{t(`GalleryPage.title2`)}
+			</div>
+
+			<div className="gallery__container">
+				<img className='img-python' src={python1} alt="img" />
+				<img className='img-python' src={python2} alt="img" />
+				<img className='img-python' src={python3} alt="img" />
+				<img className='img-python' src={python4} alt="img" />
+				<img className='img-python' src={python5} alt="img" />
+				<img className='img-python' src={python6} alt="img" />
+				<img className='img-python' src={python7} alt="img" />
+				<img className='img-python' src={python8} alt="img" />
+				<img className='img-python' src={python9} alt="img" />
+				<img className='img-python' src={python10} alt="img" />
+				<img className='img-python' src={python11} alt="img" />
+				<img className='img-python' src={python12} alt="img" />
+				<img className='img-python' src={python13} alt="img" />
+				<img className='img-python' src={python14} alt="img" />
+
 			</div>
 		</div>
 
